8fe75a994113c7578ce33b4783393979c9f44680
Eriza Fazli <erizzaaaaa@gmail.com>
2014-01-13 17:30:54 +0100

    one queue per client

--- run_test.py
+++ run_test.py
@@ -44,9 +44,6 @@ for hdlr in hdlrs:
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
 
-queue = Queue.Queue()
-lock = threading.Lock()
-
 
 class Client(threading.Thread):
     ''' Client thread, sends (cid, url) to the queue at a regular interval'''
@@ -55,56 +52,47 @@ class Client(threading.Thread):
         self.cid = cid
         self.url = url
         self.interval = interval
+        self.queue = Queue.Queue()
         self.stopped = False
         self.downloaded = 0
 
     def run(self):
         while not self.stopped:
-            lock.acquire()  # block other threads before adding a task
-            if queue.qsize() < q_size:
-                queue.put((self.cid, self.url))
-            lock.release()
+            self.queue.put(self.url)
             time.sleep(self.interval)
 
 
 class Worker(threading.Thread):
     ''' Worker thread, download files from url in the queue'''
-    def __init__(self):
+    def __init__(self, client):
         threading.Thread.__init__(self)
+        self.client = client
         self.stopped = False
 
     def run(self):
-        while not self.stopped or not queue.empty():
-            cur_qsize = queue.qsize()
-            if cur_qsize == 0:
-                logger.info('_, 0')
-                time.sleep(0.5)
-            else:
-                try:
-                    cid, cur_url = queue.get()
-                    _, headers = urllib.urlretrieve(cur_url, devnull)
-                    downloaded = headers.getheader('Content-Length')
-                    queue.task_done()
-                    if downloaded is not None:
-                        clients[cid].downloaded += int(downloaded)
-                        logger.info('{0}, {1}'.format(cid, cur_qsize))
-
-                except:  # just carry on if anything goes wrong
-                    continue
+        while not self.stopped or not self.client.queue.empty():
+            try:
+                logger.info('{0}, {1}'.format(self.client.cid,
+                                              self.client.queue.qsize()))
+                cur_url = self.client.queue.get(block=True)
+                _, headers = urllib.urlretrieve(cur_url, devnull)
+                downloaded = headers.getheader('Content-Length')
+                self.client.queue.task_done()
+                if downloaded is not None:
+                    self.client.downloaded += int(downloaded)
+                                        
+            except:  # just carry on if anything else goes wrong
+                continue
 
 
 def clean_quit(signum, frame):
     '''stop clients and workers, print out test reports'''
     _, _ = signum, frame
     logger.debug('interrupting...')
-    for clt in clients:
+    for clt, wrk in zip(clients, workers):
         clt.stopped = True
-    logger.debug('clients stopped')
-    worker.stopped = True
-    logger.debug('emptying queue...')
-    worker.join()
-    logger.debug('queue emptied')
-    logger.debug('worker stopped')
+        wrk.stopped = True
+    logger.debug('clients and workers stopped')
     testtime = time.time() - starttime
     mbps_scaler = 8 / testtime / 1e6
     agg_data = 0
@@ -120,13 +108,11 @@ def clean_quit(signum, frame):
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, clean_quit)
     clients = []
-    #base_url = 'http://www.freeware-guide.com/download/files/playlist.zip'
+    workers = []
+    #base_url = 'http://www.freeware-guide.com/download/files/lexunbackupsolution.zip'
     base_url = 'http://172.16.255.253/test_hobbit/hobbit_std/InternalUSEONLYTheHobbit_trailer_std-9.ts'
     wget_interval = 9  # seconds
     client_interval = 2  # seconds
-
-    worker = Worker()
-    worker.start()
 
     starttime = time.time()
 
@@ -134,11 +120,10 @@ if __name__ == '__main__':
         client = Client(i, base_url, wget_interval)
         client.start()
         clients.append(client)
+        worker = Worker(client)
+        worker.start()
+        workers.append(worker)
         time.sleep(client_interval)
 
     while True:
-        if queue.qsize() > q_size:  # works but not always timely stop the test
-            logger.warning('max queue size exceeded!')
-            clean_quit(signal.SIGINT, None)
-        else:
-            time.sleep(1)  # !!!
+        time.sleep(1)  # !!!
